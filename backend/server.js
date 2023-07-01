ce4b765c9a98cbe12333bba4bac6b222e57e0113
sreevalsan <sreevalsans02@gmail.com>
2023-07-01 11:25:53 +0530

    final update 3

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ const colors = require('colors')
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require("cors");
 
 connectDB()
 
 // console.log('hello')
 const app = express()
 
+
 //middleware
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
